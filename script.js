29a0da6c409c74914d57d7b3f05c20509f074ab3
Vilgot Smedberg <vilgot.smedberg@gmail.com>
2025-03-14 20:15:17 +0100

    Enter-function, translation.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function getGender() {
+document.getElementById("input").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        getGender();
+    }
+})
+
+async function getGender(word = null) {
     const errorMessage = document.getElementById('error-message');
     if (errorMessage) errorMessage.remove();
 
-    const requestedWord = document.getElementById("input").value;
+    const requestedWord = word || document.getElementById("input").value;
     const url = `https://www.qmez.de:8444/v1/scanner/es/s?w=${requestedWord}`;
     const proxyUrl = 'https://api.allorigins.win/get?url=';
     fetch(proxyUrl + encodeURIComponent(url))
         .then(response => response.json())
-        .then(data => {
+        .then(async data => {
             console.log(`Data: ${data.contents}`)
             const parsedData = JSON.parse(data.contents);
             const article = parsedData.article;
@@ -15,14 +21,31 @@ function getGender() {
             if (requestedWord == word) {
                 document.getElementById("genus").textContent = article;
                 document.getElementById("word").innerHTML = `${word}`;
+                const translatedWordSE = await translateWord(word, 'sv');
+                const translatedWordEN = await translateWord(word, 'en');
+                document.getElementById('translation-swedish').innerHTML = 'SE: ' + translatedWordSE;
+                document.getElementById('translation-english').innerHTML = 'EN: ' + translatedWordEN;
+                
             } else {
                 const container = document.getElementById('container');
                 const errorMessage = document.createElement('p');
-                errorMessage.textContent = 'Ord hittades inte. Prova igen.';
+                errorMessage.innerHTML = `Ord hittades inte. Menade du <a id="error-message-a" href="#" onclick="getGender('${word}')"> ${word}</a>?`;
                 errorMessage.id = 'error-message';
                 container.appendChild(errorMessage);
             }
 
         })
         .catch(error => console.error('Error:', error));
+}
+
+async function translateWord(word, targetLang) {
+    const url = `https://api.mymemory.translated.net/get?q=${word}&langpair=de|${targetLang}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data.responseData.translatedText;
+    } catch (error) {
+        console.error('Error:', error);
+        return 'Translation error';
+    }
 }
